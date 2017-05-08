e65237b9d1e6db904699a905e1482e68b351f7cf
Sam Wang <sam.zz.wang@gmail.com>
2017-05-08 15:34:45 -0700

    Todo list components

---
+++ frontend/components/todo_list/todo_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoListItem from './todo_list_item';
+
+const TodoList = ({ todos }) => (
+  <div>
+    <ul>
+      {todos.map((todo, idx) => <TodoListItem key={idx} todo={todo}/>)}
+
+    </ul>
+  </div>
+);
+
+export default TodoList;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { allTodos } from './reducers/selectors';
-import { receiveTodo, receiveTodos } from './actions/todo_actions';
 import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", function(){
