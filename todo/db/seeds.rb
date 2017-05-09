826543502b8a2223b546b4b3cd240b350c224dfd
Sam Wang <sam.zz.wang@gmail.com>
2017-05-09 11:53:14 -0700

    Add routes and StaticPages

--- todo/db/seeds.rb
+++ todo/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+t1 = Todo.create(title: "Wash dog", body: "Use shampoo", done: false)
+t2 = Todo.create(title: "Wash car", body: "Use soap", done: true)
